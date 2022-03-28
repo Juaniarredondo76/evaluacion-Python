f3ba6c4c700d9ebb4def8aded42d7c36b79709dc
401 <401@academicos.cesde.edu.co>
2022-03-28 15:21:25 -0500

    NTG001-se crean proyectos y se desarrolla el 1 y 2´punto de la evaluacion de python

---
+++ comprar.py
@@ -0,0 +1,10 @@
+contador=0
+
+print("MENU COMPRAS")
+print("*****************")
+print("1-->REGISTRAR EL NOMBRE DEL PRODUCTO")
+print("2-->MOSTRAR TODOS LOS PRODUCTOS REGISTRADOS")
+print("3-->BUSCAR UN PRODUCTO Y CAMBIAR EL NOMBRE")
+print("4-->BUSCAR POR NOMBRE EL PRODUCTO Y ELIMINARLO ")
+print("0-->SALIR")
+print("*****************")
